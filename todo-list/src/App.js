dfd5df698375a3b8e9b56518258bc8c72a74d438
white-hun <121800818+white-hun@users.noreply.github.com>
2023-03-20 17:53:00 +0900

    Update todo-list prectice

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header/Header";
+import TodoList from "./components/TodoList/TodoList";
 
+const filters = ["all", "completed", "active"];
 function App() {
-  return;
+  const [filter, setFilter] = useState(filters[0]);
+  return (
+    <div>
+      <Header filters={filters} filter={filter} onFilterChange={(filter) => setFilter(filter)} />
+      <TodoList filter={filter} />
+    </div>
+  );
 }
 
 export default App;
