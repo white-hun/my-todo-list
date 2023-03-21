278778eaf8102725ceb8b03e750fb0f5a136edd7
white-hun <exwhite@naver.com>
2023-03-21 18:24:12 +0900

    Update

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,17 +1,18 @@
-import { useState } from "react";
-import "./App.css";
-import Header from "./components/Header/Header";
-import TodoList from "./components/TodoList/TodoList";
+// import { useState } from "react";
+// import "./App.css";
+// import Header from "./components/Header/Header";
+// import TodoList from "./components/TodoList/TodoList";
 
-const filters = ["all", "completed", "active"];
-function App() {
-  const [filter, setFilter] = useState(filters[0]);
-  return (
-    <div>
-      <Header filters={filters} filter={filter} onFilterChange={(filter) => setFilter(filter)} />
-      <TodoList filter={filter} />
-    </div>
-  );
-}
+// const filters = ["all", "completed", "active"];
+// function App() {
+//   const [filter, setFilter] = useState(filters[0]);
+//   return (
+//     <div>
+//       <Header filters={filters} filter={filter} onFilterChange={(filter) => setFilter(filter)} />
+//       <TodoList filter={filter} />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+//-------------------------------------------------------------------
--- todo-list/src/components/AddTodo/AddTodo.jsx
+++ todo-list/src/components/AddTodo/AddTodo.jsx
@@ -1,21 +1,22 @@
-import React, { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+// import React, { useState } from "react";
+// import { v4 as uuidv4 } from "uuid";
 
-export default function AddTodo({ onAdd }) {
-  const [text, setText] = useState("");
-  const handleChange = (e) => setText(e.target.value);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (text.trim().length === 0) {
-      return;
-    }
-    onAdd({ id: uuidv4(), text: text, status: "active" });
-    setText("");
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="write & enter" value={text} onChange={handleChange} />
-      <button>Submit</button>
-    </form>
-  );
-}
+// export default function AddTodo({ onAdd }) {
+//   const [text, setText] = useState("");
+//   const handleChange = (e) => setText(e.target.value);
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     if (text.trim().length === 0) {
+//       return;
+//     }
+//     onAdd({ id: uuidv4(), text: text, status: "active" });
+//     setText("");
+//   };
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <input type="text" placeholder="write & enter" value={text} onChange={handleChange} />
+//       <button>Submit</button>
+//     </form>
+//   );
+// }
+//-------------------------------------------------------------------
