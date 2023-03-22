6f42d2a1c7413270bbf4e5412da2cd65d6b1e75f
white-hun <exwhite@naver.com>
2023-03-22 22:30:01 +0900

    Update my CSS

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,58 +1,16 @@
-// import { useState } from "react";
-// import "./App.css";
-// import Header from "./components/Header/Header";
-// import TodoList from "./components/TodoList/TodoList";
-
-// const filters = ["all", "completed", "active"];
-// function App() {
-//   const [filter, setFilter] = useState(filters[0]);
-//   return (
-//     <div>
-//       <Header filters={filters} filter={filter} onFilterChange={(filter) => setFilter(filter)} />
-//       <TodoList filter={filter} />
-//     </div>
-//   );
-// }
-
-// export default App;
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//-------------------------------------------------------------------
 import { useState } from "react";
 import Header from "./components/Header/Header";
+import Icon from "./components/Icon";
 import TodoList from "./components/TodoList/TodoList";
 
 const filters = ["all", "completed", "active"];
 export default function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <div>
-      <Header filters={filters} filter={filter} onChangeFilter={(filter) => setFilter(filter)} />
+    <>
+      <Header filters={filters} filter={filter} onChangeFilter={(value) => setFilter(value)} />
+      {/* <Icon /> */}
       <TodoList filter={filter} />
-    </div>
+    </>
   );
 }
--- todo-list/src/components/Todo/Todo.jsx
+++ todo-list/src/components/Todo/Todo.jsx
@@ -1,59 +1,6 @@
-// import React from "react";
-// import { BsTrash3 } from "react-icons/bs";
-
-// export default function Todo({ todo, onUpdate, onDelete }) {
-//   const { text, status } = todo;
-//   const handleChange = (e) => {
-//     const status = e.target.checked ? "completed" : "active";
-//     onUpdate({ ...todo, status: status });
-//   };
-//   const handleDelete = () => onDelete(todo);
-//   return (
-//     <div>
-//       <li>
-//         <input
-//           type="checkbox"
-//           id="checkbox"
-//           checked={status === "completed"}
-//           onChange={handleChange}
-//         />
-//         <label htmlFor="checkbox">{text}</label>
-//         <button onClick={handleDelete}>
-//           <BsTrash3 />
-//         </button>
-//       </li>
-//     </div>
-//   );
-// }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//-------------------------------------------------------------------
 import React from "react";
 import { BsTrash3 } from "react-icons/bs";
+import styles from "./Todo.module.css";
 
 export default function Todo({ todo, onUpdate, onDelete }) {
   const { text, status } = todo;
@@ -63,15 +10,18 @@ export default function Todo({ todo, onUpdate, onDelete }) {
   };
   const handleDelete = () => onDelete(todo);
   return (
-    <li>
+    <li className={styles.todo}>
       <input
+        className={styles.checkbox}
         type="checkbox"
         id="checkbox"
         checked={status === "completed"}
         onChange={handleChange}
       />
-      <label htmlFor="checkbox">{text}</label>
-      <button onClick={handleDelete}>
+      <label htmlFor="checkbox" className={styles.text}>
+        {text}
+      </label>
+      <button className={styles.button} onClick={handleDelete}>
         <BsTrash3 />
       </button>
     </li>
