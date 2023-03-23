4b160b083610c8b97de9b877cb78a2122398bd7a
white-hun <121800818+white-hun@users.noreply.github.com>
2023-03-23 17:57:22 +0900

    Update DarkModeContext.jsx

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const DarkModeContext = createContext();
 
 export default function DarkModeProvider({ children }) {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
   const toggle = () => setDarkMode((mode) => !mode);
 
   return (
