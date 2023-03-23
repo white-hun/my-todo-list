15bdfb2832fe17380f06c8aec459a214fa2493c7
white-hun <121800818+white-hun@users.noreply.github.com>
2023-03-23 14:17:51 +0900

    Update

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header/Header";
 import Icon from "./components/Icon";
 import TodoList from "./components/TodoList/TodoList";
 
-const filters = ["all", "completed", "active"];
+const filters = ["all", "active", "completed"];
 export default function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
