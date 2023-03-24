1090b426bdb7cb1a995d9a3c98382d543ae2e46a
white-hun <121800818+white-hun@users.noreply.github.com>
2023-03-24 14:44:35 +0900

    Update CSS Spring_ver

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import logo from "../logo.svg";
+import logo2 from "../logo2.svg";
+import logoRed from "../logoRed.svg";
 import "../App.css";
+import { useDarkMode } from "../context/DarkModeContext";
 
 function Icon(props) {
+  const { darkMode } = useDarkMode();
   return (
     <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
+      {/* <img src={logoRed} className="App-logo" alt="logo" /> */}
+      {darkMode ? (
+        <img src={logo2} className="App-logo" alt="logo" />
+      ) : (
+        <img src={logo} className="App-logo" alt="logo" />
+      )}
     </div>
   );
 }
