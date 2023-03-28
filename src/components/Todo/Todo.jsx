ffb8835b78dab70717ed342ba5be6bb3fd35aa4f
white-hun <121800818+white-hun@users.noreply.github.com>
2023-03-28 13:54:57 +0900

    Update Todo.jsx

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -27,11 +27,3 @@ export default function Todo({ todo, onUpdate, onDelete }) {
     </li>
   );
 }
-
-// export const todayFormal = () => {
-//   let now = new Date();
-//   let todayYear = now.getFullYear();
-//   let todayMonth = (now.getMonth() + 1) > 9? (now.getMonth() + 1) : '0' + (now.getMonth() + 1);
-//     let todayDate = now.getDate() > 9 ? now.getDate() : '0' + now.getDate();
-//     return todayYear + '-' + todayMonth + '-' + todayDate;
-//   }
